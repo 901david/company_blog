27b686164f46fdd82ddcb477dbd290369ca4bd00
901david <vdavidhammond@gmail.com>
2018-03-06 15:52:48 -0700

    started setting up express server and auith routes, added in needed packages and started setting up Github environment varibles

--- server.js
+++ server.js
@@ -1,24 +1,21 @@
-// Dependencies
-// =============================================================
+require('dotenv').config();
 const express = require("express");
 const bodyParser = require("body-parser");
-const path = require('path');
-
-
-// Sets up the Express App
-// =============================================================
 const app = express();
 const PORT = process.env.PORT || 3002;
+const passport = require('passport');
 
-// Sets up the Express app to handle data parsing
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.text());
 app.use(bodyParser.json({ type: "application/vnd.api+json" }));
-
-require("./authroutes.js")(app);
-require("./apiroutes.js")(app);
-
+app.use(require('cookie-parser')());
+app.use(require('body-parser').urlencoded({ extended: true }));
+app.use(require('express-session')({ secret: 'keyboard cat', resave: true, saveUninitialized: true }));
+app.use(passport.initialize());
+app.use(passport.session());
+require("./routes/authroutes.js")(app);
+require("./routes/apiroutes.js")(app);
 
 // Server Listener
 app.listen(PORT, function() {
